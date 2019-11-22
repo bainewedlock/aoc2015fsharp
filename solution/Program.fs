e12f769af7d826fe84feeaedb8b51f5f9115f023
Baine Wedlock <bainewedlock@gmail.com>
2019-11-22 21:30:44 +0100

    x

--- solution/Program.fs
+++ solution/Program.fs
@@ -1,10 +1,9 @@
 ﻿open System
 
-
 [<EntryPoint>]
 let main argv =
+    printfn "day 1: %d %d" day_1.answer day_1.answer'
+    printfn "day 2: %d %d" day_2.answer day_2.answer'
     0 // return an integer exit code
-
-
 
 
