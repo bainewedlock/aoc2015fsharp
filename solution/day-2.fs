f448dc9638e129087a10ce68a3f4ddaadb0944fd
Baine Wedlock <bainewedlock@gmail.com>
2019-11-22 21:28:12 +0100

    day2b

--- solution/day-2.fs
+++ solution/day-2.fs
@@ -10,8 +10,9 @@ let split_lines (s:String) =
 let split (delim:Char) (s:String) =
     s.Split(delim)
 
-let calc_surface a b c =
-    3 * a * b + 2 * b * c + 2 * a * c
+let calc_paper = function
+    | [l;w;h] -> 3 * l * w + 2 * w * h + 2 * l * h
+    | _ -> failwith "cant calc paper"
 
 let parse_line line =
     Regex.Match(line, @"(\d+)x(\d+)x(\d+)").Groups
@@ -20,11 +21,23 @@ let parse_line line =
     |> Seq.map (fun g -> g.Value |> Convert.ToInt32) 
     |> Seq.sort
     |> Seq.toList
-    |> function
-        | [a; b; c] -> calc_surface a b c
 
 let answer =
     read_input 2
     |> split_lines
     |> Array.map parse_line
+    |> Array.map calc_paper
     |> Seq.sum
+
+let calc_ribbon = function
+    | [l;w;h] -> 2 * (l + w) + l*w*h
+    | _ -> failwith "cant calc ribbon"
+
+let answer' =
+    read_input 2
+    |> split_lines
+    |> Array.map parse_line
+    |> Array.map calc_ribbon
+    |> Seq.sum
+
+
