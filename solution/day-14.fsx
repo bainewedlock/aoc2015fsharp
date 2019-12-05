0d81a99f04a07f0fe9e47009fbc661b83322aa03
Baine Wedlock <bainewedlock@gmail.com>
2019-12-06 00:53:03 +0100

    day-14 part 1

---
+++ solution/day-14.fsx
@@ -0,0 +1,20 @@
+﻿#load "input.fsx"
+
+open System.Text.RegularExpressions
+open System.Linq
+
+let parse line =
+  let mx = Regex.Matches(line, @"\d+").Cast<Match>() |> Seq.toList
+  match mx |> List.map (fun x -> int x.Value) with
+  | [fly;sec;sleep] ->
+    seq { while true do
+          yield! (Seq.replicate sec fly)
+          yield! (Seq.replicate sleep 0) }
+  
+let answer =
+  Input.lines "14"
+  |> Array.toList
+  |> List.map parse
+  |> List.map (Seq.take 2503 >> Seq.sum)
+  |> List.max
+
