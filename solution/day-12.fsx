2d90ef34cec07aa1e3889ecdbb217254fff2179f
Baine Wedlock <bainewedlock@gmail.com>
2019-12-05 22:48:37 +0100

    refac

--- solution/day-12.fsx
+++ solution/day-12.fsx
@@ -11,21 +11,18 @@ let answer =
 #r @"C:/users/wboec/.nuget/packages/fsharp.data/3.3.2/lib/net45/FSharp.Data.dll"
 open FSharp.Data
 
-let hasRed = function
-  | (_, JsonValue.String x) when x = "red" -> true
+let hasStringValue v = function
+  | (_, JsonValue.String x) when x = v -> true
   | _ -> false
+
 let rec extract node =
   match node with
-  | JsonValue.Array xs ->
-    xs |> Array.toList |> List.collect extract
+  | JsonValue.Array xs -> xs |> Array.sumBy extract
   | JsonValue.Record xs ->
-    if xs |> Array.exists hasRed then []
-    else xs |> Array.toList |> List.collect (fun (_, v) -> extract v)
-  | JsonValue.Number x -> [x]
-  | _ -> []
-
+      if xs |> Array.exists (hasStringValue "red") then 0
+      else xs |> Array.sumBy (fun (_, v) -> extract v)
+  | JsonValue.Number x -> int x
+  | _ -> 0
 let answer' =
   JsonValue.Parse(Input.text "12")
   |> extract
-  |> List.sum
-
