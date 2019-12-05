7146459b8b356cbfc35003f18d6e9b87738733bf
Baine Wedlock <bainewedlock@gmail.com>
2019-12-05 21:27:45 +0100

    refac

--- solution/day-11.fsx
+++ solution/day-11.fsx
@@ -1,31 +1,21 @@
 ﻿#load "input.fsx"
-
 open System
 
 let alphabet = ['a'..'z'] |> List.except ['i';'o';'l'] |> List.indexed
 let encodeMap = alphabet |> List.map (fun (a,b) -> (b,a)) |> Map 
 let decodeMap = alphabet |> Map
 
-let findSequence offset numbers =
-  numbers
-  |> List.scan (fun acc x -> 
-    match acc with
-    | (n, x') when x = x' + offset ->
-      (n+1, x)
-    | _ ->
-      (1, x)) (0, 0)
-    |> List.tail
-    |> List.map fst
+let hasStraightOfThree =
+  List.windowed 3 >>
+  List.exists (fun [a;b;c] ->
+    a = b + 1 && b = c + 1)
 
-let hasStraightOfThree numbers =
-  findSequence -1 numbers
-  |> List.contains 3
-
-let hasTwoPairs numbers =
-  findSequence 0 numbers
-  |> List.filter (fun n -> n = 2)
-  |> List.length
-  |> fun x -> x > 1
+let hasTwoPairs =
+  List.windowed 2 >>
+  List.filter (fun [a;b] -> 
+    a = b) >>
+  List.distinct >>
+  (fun x -> x.Length >= 2)
 
 let valid numbers =
   hasStraightOfThree numbers
@@ -48,18 +38,15 @@ let next = function
   | bad ->
     failwithf "unexpected: '%A'" bad
 
-let rec nextUntil counter f numbers =
-  let test = next numbers
-  if f test then test
-  else 
-    nextUntil (counter + 1) f test
-
 let answer password =
   password
   |> Seq.toList
   |> List.rev
   |> List.map (fun x -> encodeMap.Item(x))
-  |> nextUntil 0 valid
+  |> Seq.unfold (fun numbers ->
+    let candidate = next numbers
+    Some (candidate, candidate))
+  |> Seq.find valid
   |> List.map (fun x -> decodeMap.Item(x))
   |> List.rev
   |> fun chars -> String.Concat(chars)
