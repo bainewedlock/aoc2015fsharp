36c7de8b24d3f0ed4712277e76d5be028c2280c3
Baine Wedlock <bainewedlock@gmail.com>
2019-12-06 20:34:47 +0100

    day-17

---
+++ solution/day-17.fsx
@@ -0,0 +1,29 @@
+﻿#load "input.fsx"
+
+let rec permute n acc cs = [
+  if n = 0 then yield acc
+  else
+    let cs' = cs |> List.filter ((>=) n)
+    for (i, p) in cs' |> List.indexed do
+      let rest = List.skip (i+1) cs'
+      yield! permute (n-p) (p::acc) rest
+]
+
+let solve input x =
+  Input.asList input
+  |> List.map int
+  |> permute x []
+  |> List.length
+solve "sample-17" 25
+solve "input-17" 150
+
+let solve' input x =
+  Input.asList input
+  |> List.map int
+  |> permute x []
+  |> List.countBy List.length
+  |> List.minBy fst
+  |> snd
+solve' "sample-17" 25
+solve' "input-17" 150
+
