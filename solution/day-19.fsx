c402b5e3ec1f281e4608ff8c2c5c58e90c913923
Baine Wedlock <bainewedlock@gmail.com>
2019-12-07 15:07:56 +0100

    refac

--- solution/day-19.fsx
+++ solution/day-19.fsx
@@ -7,48 +7,52 @@ let goal = "e"
 
 type Rule =
   { left : string; right : string }
-  with static member invert rule =
+  with static member reverse rule =
         { left = rule.right; right = rule.left}
 
-let (|Rule|_|) input =
-  match Regex.Match(input, @"(\w+) => (\w+)") with
+let (|Rule|_|) s =
+  match Regex.Match(s, @"(\w+) => (\w+)") with
   | m when m.Success ->
-    Some { left = m.Groups.[1].Value; right = m.Groups.[2].Value }
+    Some {
+        left = m.Groups.[1].Value;
+        right = m.Groups.[2].Value }
   | _ -> None
 
-let replace (s : string) i n (r : string) =
-  s.Substring(0, i) + r + s.Substring(i + n)
+module Rule =
+  let private replaceSubstring (s : string) i l (r : string) =
+    s.Substring(0, i) + r + s.Substring(i + l)
 
-let rec apply (i : int) (molecule : string) rule  = [
-  match molecule.IndexOf(rule.left, i) with
-  | -1 -> ()
-  | i' ->
-    let n = rule.left.Length
-    yield replace molecule i' n rule.right
-    yield! apply (i' + 1) molecule rule 
-]
+  let rec apply (i : int) (molecule : string) rule  = [
+    match molecule.IndexOf(rule.left, i) with
+    | -1 -> ()
+    | i' ->
+      let n = rule.left.Length
+      yield replaceSubstring molecule i' n rule.right
+      yield! apply (i' + 1) molecule rule 
+  ]
 
-let rec applyAll (rules : Rule list) molecule = Set [
-  match rules with
-  | [] -> ()
-  | hd::tail ->
-    yield! apply 0 molecule hd
-    yield! applyAll tail molecule
-]
+  let rec applyAll (rules : Rule list) molecule = Set [
+    match rules with
+    | [] -> ()
+    | hd::tail ->
+      yield! apply 0 molecule hd
+      yield! applyAll tail molecule
+  ]
 
-let parse lines = 
-  lines
-  |> List.filter ((<>) "")
-  |> List.fold (fun (rules, molecule) -> function
-    | Rule r -> (r::rules, molecule)
-    | m -> (rules, Some m)) ([], None)
-  |> function
-  | rules, Some molecule -> (rules, molecule)
-  | _, None -> failwithf "Could not find molecule in input"
+  let parseLines lines = 
+    lines
+    |> List.filter ((<>) "")
+    |> List.fold (fun (rules, m) -> function
+      | Rule r -> (r::rules, m)
+      | molecule -> (rules, Some molecule))
+        ([], None)
+    |> function
+    | rules, Some molecule -> (rules, molecule)
+    | _, None -> failwithf "Could not find molecule in input"
 
-let solve input = 
-  let (rules, molecule) = parse (Input.asList input)
-  applyAll rules molecule
+let solve file = 
+  let (rules, molecule) = Rule.parseLines (Input.asList file)
+  Rule.applyAll rules molecule
   |> Set.count
 
 let sample = solve "sample-19"
@@ -59,15 +63,19 @@ let simplify input =
     [|'a'..'z'|]
     |> Array.append [|'A'..'Z'|]
     |> Array.append [|'0'..'9'|]
-  parse (Input.asList input)
+    |> Array.except [| char goal |]
+  Rule.parseLines <| Input.asList input
   |> fst
   |> List.map (fun r -> r.left)
   |> List.distinct
-  |> List.mapi (fun i x -> { left = x; right = string alphabet.[i] })
-  |> List.sortByDescending (fun r -> r.left.Length)
-  |> List.filter (fun r -> r.left <> goal)
-  |> List.fold (fun (acc : string) r -> acc.Replace(r.left, r.right)) (Input.asText input)
-  |> fun text -> text.Split([|"\r\n"|], StringSplitOptions.None) 
+  |> List.mapi (fun i x ->
+    { left = x; right = string alphabet.[i] })
+  |> List.filter (fun r ->
+    r.left <> goal)
+  |> List.fold (fun (acc : string) r ->
+    acc.Replace(r.left, r.right)) (Input.asText input)
+  |> fun text ->
+    text.Split([|"\r\n"|], StringSplitOptions.None) 
   |> Array.toList
 
 let rec solutions steps history rules (m : string) = seq {
@@ -77,7 +85,7 @@ let rec solutions steps history rules (m : string) = seq {
   | m ->
     let ms' =
       rules
-      |> List.collect (apply 0 m)
+      |> List.collect (Rule.apply 0 m)
       |> Set
       |> Set.filter (fun m' -> m'.Length <= m.Length)
     let history' = history.Add m
@@ -87,15 +95,18 @@ let rec solutions steps history rules (m : string) = seq {
   }
 
 let solve' input = 
-  let (rules, molecule) = parse input
-  let rules' = rules |> List.map Rule.invert
-  solutions 0 Set.empty rules' molecule
-  |> Seq.fold (fun min x ->
-    match min with
-    | _ when x < min || min = -1 ->
-      printfn "found solution with %d steps @ %A, continuing..." x DateTimeOffset.Now
-      x
-    | min -> min) -1
+  let (rules, molecule) = Rule.parseLines input
+  let rules' = rules |> List.map Rule.reverse
+  let result =
+    solutions 0 Set.empty rules' molecule
+    |> Seq.fold (fun min x ->
+      if x < min || min = -1 then
+        printfn "found %d steps @ %A, still running..." x DateTimeOffset.Now
+        x
+      else
+        min) -1
+  printfn "done"
+  result
   
 let sample' = solve' (Input.asList "sample-19")
 
