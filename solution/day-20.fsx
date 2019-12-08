71e0b195dbf1c2255f845b0b6a1ea846f7ad078f
Baine Wedlock <bainewedlock@gmail.com>
2019-12-09 00:03:16 +0100

    refac (faster without primes)

--- solution/day-20.fsx
+++ solution/day-20.fsx
@@ -4,43 +4,21 @@
 
 let intRoot x = float x |> sqrt |> int
 
-let isPrime x =
-  match x with
-  | 1 -> false
-  | _ ->
-    {2..intRoot x}
-    |> Seq.forall (fun t -> x % t > 0)
-
-let primesUpto x = 
-  [2..x]
-  |> List.filter isPrime
-
-let divisors primes x =
+let divisors x =
   let max = intRoot x
-  primes
-  |> List.takeWhile ((>=) max)
-  |> List.collect (fun p -> [p..p..max])
-  |> List.collect (fun i ->
-    match x/i with
-    | _ when x % i > 0 -> []
-    | n when n = i -> [n]
-    | n -> [i; n])
-  |> List.append [1; x]
-  |> List.distinct
+  [ 1..max ]
+  |> List.filter  (fun x' -> x % x' = 0)
+  |> List.collect (fun x' -> [x'; x/x'])
 
-let inifiteElves x x' = true
-let limitedElves x x' = x / x' <= 50
 let solve fElf factor n =
-  let primes = primesUpto <| intRoot n
   let house x =
-    divisors primes x
+    divisors x
     |> List.filter (fElf x)
     |> List.sum
     |> fun s -> s * factor
   let rec loop x =
     match house x with
-    | h when h >= n ->
-      (x, h)
+    | h when h >= n -> (x, h)
     | h ->
       if x % 100000 = 0 then
         printfn "%d->%d is too low @ %A"
@@ -51,13 +29,15 @@ let solve fElf factor n =
 let input = Input.asText "input-20" |> int
  
 #time
-let answer = solve inifiteElves 10 34000000
+let inifiteElves x x' = true
+let answer = solve inifiteElves 10 input
 #time
-//val it : int * int = (786240, 34137600)
+//Real: 00:00:12.423, CPU: 00:00:12.375, GC gen0: 10335, gen1: 1, gen2: 0
+//val answer : int * int = (786240, 34137600)
 
 #time
-let answer' = solve limitedElves 11 34000000
+let limitedElves x x' = x / x' <= 50
+let answer' = solve limitedElves 11 input
 #time
-//Real: 00:00:56.527, CPU: 00:00:55.781, GC gen0: 51408, gen1: 8, gen2: 1
-//val solve : fElf:'a -> factor:int -> n:int -> start:int -> int * int
+//Real: 00:00:11.902, CPU: 00:00:11.875, GC gen0: 11065, gen1: 1, gen2: 0
 //val answer' : int * int = (831600, 35780206)
